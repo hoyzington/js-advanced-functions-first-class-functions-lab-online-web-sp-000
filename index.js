1c7f4315cf1fbb91e81c6a44f9344f8041e6adc8
Scott Hoisington <hoyzington@gmail.com>
2020-09-23 05:19:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const returnFirstTwoDrivers = function (driversArray) {
-  
+  return driversArray.slice(0,2);
 };
 
 const returnLastTwoDrivers = function (driversArray) {
-  
+  return driversArray.slice(-2);
 };
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
