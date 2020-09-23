22d1353f0ba13d727ea17e2ca8cc446eee869cd5
Scott Hoisington <hoyzington@gmail.com>
2020-09-23 05:13:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const returnFirstTwoDrivers = function (driversArray) {};
-const returnLastTwoDrivers = function (driversArray) {};
+const returnFirstTwoDrivers = function (driversArray) {
+  
+};
+
+const returnLastTwoDrivers = function (driversArray) {
+  
+};
+
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
 function createFareMultiplier(num) {}
+
 const fareDoubler = function (fare) {};
+
 const fareTripler = function (fare) {};
+
 function selectDifferentDrivers(driversArray, driversFunction) {}
